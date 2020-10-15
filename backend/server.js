86148b399235c77af7111cab4f92bc4e2e9f940a
Gustavo Mata <gustavo.mata@clouddog.com.br>
2020-10-14 22:50:55 -0300

    adding backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const { Socket } = require('dgram')
+const app = require('express')()
+const port = 5000
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+let messages = []
+io.on('connection', (socket) => {
+  console.log('Conected...', socket.id)
+
+  socket.emit('previousMessages', messages)
+  socket.on('sendMessage', data => {
+    messages.push(data)
+    socket.broadcast.emit('receivedMessage', data)
+  })
+})
+
+http.listen(port, () => {
+  console.log('Listening... Port:', port)
+})
