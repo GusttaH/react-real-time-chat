d43a00f5c589ad55de4badada0fec8e304afe92f
Gustavo Mata <gustavo.mata@clouddog.com.br>
2020-10-15 08:54:31 -0300

    feat(frontend): adding userName

--- frontend/real-time-chat/src/App.js
+++ frontend/real-time-chat/src/App.js
@@ -6,14 +6,29 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 const socket = io.connect('http://localhost:5000')
 
-const Modal = () => {
-
+const Modal = ({ setUser }) => {
+  const [userName, setUserName] = useState('')
   return (
     <>
-      <div className="position-absolute w-100 vh-100 d-flex align-items-center">
-        <div className="container h-100">
-          <div className="card">
-            ADASDASD
+      <div className="position-absolute w-100 vh-100" style={{zIndex: 10, background: '#00000065'}}>
+        <div className="container h-100 d-flex align-items-center justify-content-center">
+          <div className="card w-50" style={{ background: '#cdc9c3' }}>
+            <div className="card-header" style={{ background: '#fbf7f0'}}>
+              <h4 className="text-muted mb-0">Bem vindo ao Bate Papo!</h4>
+            </div>
+            <div className="card-body">
+              <form onSubmit={(e) => {
+                e.preventDefault()
+                console.log(userName)
+                userName.length && setUser(userName)
+              }}>
+                <input 
+                  type="text" 
+                  className="rounded border-0 p-2 w-100" 
+                  placeholder="Escolha um nome de usuário..." 
+                  onChange={(e) => setUserName(e.target.value)} />
+              </form>
+            </div>
           </div>
         </div>
       </div>
@@ -42,7 +57,7 @@ const  App = () => {
   const sendMessage = (e) => {
     e.preventDefault()
     const message = {
-      user: 'Gustavo',
+      user: user,
       message: currentMessage
     }
     socket.emit('sendMessage', message)
@@ -52,7 +67,7 @@ const  App = () => {
 
   return (
     <>
-      <Modal/>
+      {!user && <Modal setUser={setUser} /> }
       <div className="w-100 vh-100" style={{ background: '#d9e4dd'}}>
         <div className="container d-flex align-items-center h-100 justify-content-center">
           <div className="card h-75 w-75" style={{ background: '#fbf7f0', borderRadius: '15px' }}>
@@ -61,7 +76,11 @@ const  App = () => {
                 {
                   messages.map(message => {
                     return(
-                      <p className="p-3 rounded border w-50 shadow-sm">{message.message}</p>
+                      <div className="rounded border w-50 px-3 pb-2 pt-0 shadow-sm mb-3">
+                        <span style={{ fontSize: '.8rem', fontWeight: 'bold', color: '#555555'}} >{message.user}</span>
+                        <hr className="mt-0 mb-2" />
+                        <p className="mb-0" style={{ color: '#555555' }}>{message.message}</p>
+                      </div>
                     )
                   })
                 }
